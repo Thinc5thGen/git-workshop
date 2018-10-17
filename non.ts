fa92bc86f8f0ff52a2575afe6f1bc786f508d1dd
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-10-17 17:38:53 +0700

    :tada: Hello World

---
+++ non.ts
@@ -0,0 +1,6 @@
+const firstname = "Chutipon";
+const lastname = "Hirankanokkul";
+const nickname = "Non";
+const thinc = 4;
+console.log(`My name is ${firstname} ${lastname}\n My nickname is ${nickname}. Thinc. gen ${thinc}`);
+
